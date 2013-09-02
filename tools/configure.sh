8023584fb05e0ddb4c00703952a2426953179495
Bhavesh Mehta <bhavesh049@gmail.com>
2013-09-02 01:37:15 -0700

    get clang to work part1

--- tools/configure.sh
+++ tools/configure.sh
@@ -16,8 +16,10 @@ sudo cp $REDLOCUS_GEARS_DIR/tools/C/cloc $REDLOCUS_TOOLS_DIR/cloc
 sudo cp $REDLOCUS_GEARS_DIR/tools/C/pmccabe $REDLOCUS_TOOLS_DIR/pmccabe
 sudo cp $REDLOCUS_GEARS_DIR/tools/C/comments_tool.py $REDLOCUS_TOOLS_DIR/comments_tool.py
 sudo cp -R $REDLOCUS_GEARS_DIR/tools/C/pmd-bin-5.0.5 $REDLOCUS_TOOLS_DIR/pmd-bin-5.0.5
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/lcov/geninfo $REDLOCUS_TOOLS_DIR/geninfo
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/lcov/genhtml $REDLOCUS_TOOLS_DIR/genhtml
+sudo mkdir -p $REDLOCUS_TOOLS_DIR/lcov
+sudo cp $REDLOCUS_GEARS_DIR/tools/C/lcov/geninfo $REDLOCUS_TOOLS_DIR/lcov/geninfo
+sudo cp $REDLOCUS_GEARS_DIR/tools/C/lcov/genhtml $REDLOCUS_TOOLS_DIR/lcov/genhtml
+sudo cp $REDLOCUS_GEARS_DIR/tools/C/clang/clang $REDLOCUS_TOOLS_DIR/clang
 
 sudo rm -rf $REDLOCUS_PARSERS_DIR
 sudo mkdir -p $REDLOCUS_PARSERS_DIR
@@ -25,3 +27,4 @@ sudo mkdir -p $REDLOCUS_PARSERS_DIR
 sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/mcc_parser.py $REDLOCUS_PARSERS_DIR/mcc_parser.py
 sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/cpd_parser.py $REDLOCUS_PARSERS_DIR/cpd_parser.py
 sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/lcov_parser.py $REDLOCUS_PARSERS_DIR/lcov_parser.py
+sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/clang_parser.py $REDLOCUS_PARSERS_DIR/clang_parser.py
