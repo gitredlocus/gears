81944ca5dad990c1b702eedd2c9e09b367d04dff
Bhavesh Mehta <bhavesh049@gmail.com>
2013-09-07 13:28:41 -0700

    database config for sedash

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ language tools, jenkins scripts, conversion scripts, setup scripts
 To install:
 1) Get a linux box. (or a VM, or a Linode)
 2) Clone the gears repo.
-3) Run setup/install.sh (This installs Apache, Mysql, PHP, and Jenkins)
-4) Run setup/jenkins/configure.sh (This configures jenkins jobs, users, and sets up tools for the jobs)
-5) sudo cp -R web/ /var/www
-6) sudo service apache2 restart
-7) sudo chmod a+w /var/www/web/protected/runtime
-8) Remove the gears repo
+3) Run setup/install.sh (This installs Apache, Mysql, PHP, and Jenkins) (Choose root:root as login for mysql)
+4) mysql -u root -p < setup/configdb.sql (enter password for root)
+5) Run setup/jenkins/configure.sh (This configures jenkins jobs, users, and sets up tools for the jobs)
+6) sudo cp -R web/ /var/www
+7) sudo service apache2 restart
+8) sudo chmod a+w /var/www/web/protected/runtime
+9) Remove the gears repo
 
 To uninstall:
 1) Clone the gears repo on the linux box.
 2) Run setup/jenkins/unconfigure.sh
-3) Run setup/uninstall.sh
-4) sudo rm -rf /var/www/web
-5) Remove gears repo
+3) mysql -u root -p < setup/unconfigdb.sql (enter password for root)
+4) Run setup/uninstall.sh
+5) sudo rm -rf /var/www/web
+6) Remove gears repo
---
+++ setup/unconfigdb.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 07, 2013 at 07:15 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `sedash`
+--
+
+USE `sedash`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_info`
+--
+
+DROP TABLE `group_info`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE `project`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_config_info`
+--
+
+DROP TABLE `project_config_info`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_tool_info`
+--
+
+DROP TABLE `project_tool_info`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `run_info`
+--
+
+DROP TABLE `run_info`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tool_info`
+--
+
+DROP TABLE `tool_info`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tool_run_info`
+--
+
+DROP TABLE `tool_run_info`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE `user`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group_info`
+--
+
+DROP TABLE `user_group_info`;
+
+
+-- --------------------------------------------------------
+
+--
+-- Database `sedash`
+--
+
+DROP DATABASE `sedash`;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
