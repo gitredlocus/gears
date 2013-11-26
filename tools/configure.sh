692475b255a507ff33f89f78701f59218a024cdf
Bhavesh Mehta <bhavesh.bus@gmail.com>
2013-11-25 22:29:43 -0800

    fix tools configuration script

--- tools/configure.sh
+++ tools/configure.sh
@@ -5,26 +5,26 @@
 #             copies parsers in /opt/redlocus/parsers directory
 #
 
-REDLOCUS_GEARS_DIR=/home/bmehta/redlocus/gears
-REDLOCUS_TOOLS_DIR=/opt/redlocus/tools
-REDLOCUS_PARSERS_DIR=/opt/redlocus/parsers
+REDLOCUS_TOOLS_DIR=$PWD
+TOOLS_INSTALLATION_DIR=/opt/redlocus/tools
+PARSERS_INSTALLATION_DIR=/opt/redlocus/parsers
 
-sudo rm -rf $REDLOCUS_TOOLS_DIR
-sudo mkdir -p $REDLOCUS_TOOLS_DIR
+sudo rm -rf $TOOLS_INSTALLATION_DIR
+sudo mkdir -p $TOOLS_INSTALLATION_DIR
 
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/cloc $REDLOCUS_TOOLS_DIR/cloc
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/pmccabe $REDLOCUS_TOOLS_DIR/pmccabe
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/comments_tool.py $REDLOCUS_TOOLS_DIR/comments_tool.py
-sudo cp -R $REDLOCUS_GEARS_DIR/tools/C/pmd-bin-5.0.5 $REDLOCUS_TOOLS_DIR/pmd-bin-5.0.5
-sudo mkdir -p $REDLOCUS_TOOLS_DIR/lcov
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/lcov/geninfo $REDLOCUS_TOOLS_DIR/lcov/geninfo
-sudo cp $REDLOCUS_GEARS_DIR/tools/C/lcov/genhtml $REDLOCUS_TOOLS_DIR/lcov/genhtml
-sudo cp -R $REDLOCUS_GEARS_DIR/tools/C/clang/llvm $REDLOCUS_TOOLS_DIR/llvm
+sudo cp $REDLOCUS_TOOLS_DIR/C/cloc $TOOLS_INSTALLATION_DIR/cloc
+sudo cp $REDLOCUS_TOOLS_DIR/C/pmccabe $TOOLS_INSTALLATION_DIR/pmccabe
+sudo cp $REDLOCUS_TOOLS_DIR/C/comments_tool.py $TOOLS_INSTALLATION_DIR/comments_tool.py
+sudo cp -R $REDLOCUS_TOOLS_DIR/C/pmd-bin-5.0.5 $TOOLS_INSTALLATION_DIR/pmd-bin-5.0.5
+sudo mkdir -p $TOOLS_INSTALLATION_DIR/lcov
+sudo cp $REDLOCUS_TOOLS_DIR/C/lcov/geninfo $TOOLS_INSTALLATION_DIR/lcov/geninfo
+sudo cp $REDLOCUS_TOOLS_DIR/C/lcov/genhtml $TOOLS_INSTALLATION_DIR/lcov/genhtml
+sudo cp -R $REDLOCUS_TOOLS_DIR/C/clang/llvm $TOOLS_INSTALLATION_DIR/llvm
 
-sudo rm -rf $REDLOCUS_PARSERS_DIR
-sudo mkdir -p $REDLOCUS_PARSERS_DIR
+sudo rm -rf $PARSERS_INSTALLATION_DIR
+sudo mkdir -p $PARSERS_INSTALLATION_DIR
 
-sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/mcc_parser.py $REDLOCUS_PARSERS_DIR/mcc_parser.py
-sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/cpd_parser.py $REDLOCUS_PARSERS_DIR/cpd_parser.py
-sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/lcov_parser.py $REDLOCUS_PARSERS_DIR/lcov_parser.py
-sudo cp $REDLOCUS_GEARS_DIR/tools/parsers/clang_parser.py $REDLOCUS_PARSERS_DIR/clang_parser.py
+sudo cp $REDLOCUS_TOOLS_DIR/parsers/mcc_parser.py $PARSERS_INSTALLATION_DIR/mcc_parser.py
+sudo cp $REDLOCUS_TOOLS_DIR/parsers/cpd_parser.py $PARSERS_INSTALLATION_DIR/cpd_parser.py
+sudo cp $REDLOCUS_TOOLS_DIR/parsers/lcov_parser.py $PARSERS_INSTALLATION_DIR/lcov_parser.py
+sudo cp $REDLOCUS_TOOLS_DIR/parsers/clang_parser.py $PARSERS_INSTALLATION_DIR/clang_parser.py
